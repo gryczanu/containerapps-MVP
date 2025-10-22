e47f0192217b8213dac336a89d32a8ba52647284
Urszula Gryczan <gryczanu@gmail.com>
2025-10-22 15:02:09 +0200

    wiki creations

--- iac/scripts/publish-wiki.sh
+++ iac/scripts/publish-wiki.sh
@@ -11,8 +11,6 @@ pageContent=$(cat wiki.md)
 releaseDir="$organizationUrl$project/_apis/wiki/wikis/albums/pages?path=Releases/$releaseName"
 pageUrlPostfix="&versionDescriptor.version=main&api-version=7.1"
 pageName="ACA-doc-$date"
-
-releaseDir="$organizationUrl$project/_apis/wiki/wikis/ablums/pages?path=Releases/$releaseName"
 pageUrlPostfix="&versionDescriptor.version=main&api-version=7.1"
 
 # Create release wiki page
